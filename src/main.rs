b1020598921d53cb302b8f04adf4b4b2f2e3aea3
teackot <k.qovekt@gmail.com>
2021-12-12 19:38:10 +0300

    Remove commented code

--- src/main.rs
+++ src/main.rs
@@ -23,14 +23,6 @@ fn main() -> Result<(), &'static str> {
     let fname = args[1].clone();
 
     let mut comp = Computer::new(256);
-    // put a, 5; put c, 2; sub a, c;
-    // put b, 3; cmp a, b; je 4;
-    // halt
-    // comp.load_program(vec![
-    //     0b01001000, 5, 0b01001010, 2, 0b00010000, 0b010,
-    //     0b01001001, 3, 0b01110000, 0b001, 0b10100000, 4,
-    //     0b01101000,
-    // ]);
     
     let a = Assembler::new();
     comp.load_program(a.assemble(&fname));
